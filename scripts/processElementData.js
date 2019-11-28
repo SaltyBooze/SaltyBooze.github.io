a2d381501296c7401ab7951b10f3a36b074bbf0c
SaltyBooze <entradadedocumentos@hotmail.com>
2019-11-27 22:18:18 -0300

    First Commit!
    Pushing the BDD stuff to the github

---
+++ scripts/processElementData.js
@@ -0,0 +1,50 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_hdKc0RFbEeqnVve3NPiUeQActivity"]="./../../BDD/deliveryprocesses/resources/Processo BDD_1733CE56_1733ce56_Activity.jpeg"
+contentPage.processPage.imageFiles["_hdKc0RFbEeqnVve3NPiUeQ,_s_f3kRFbEeqnVve3NPiUeQActivity"]="./../../BDD/deliveryprocesses/resources/Formular_CBB46CA0_d05ca74a_Activity.jpeg"
+contentPage.processPage.imageFiles["_hdKc0RFbEeqnVve3NPiUeQ,_oDvIMRFbEeqnVve3NPiUeQActivity"]="./../../BDD/deliveryprocesses/resources/Ilustrar_7B24EA6D_7fcd2517_Activity.jpeg"
+contentPage.processPage.imageFiles["_hdKc0RFbEeqnVve3NPiUeQ,_yd4L0RFbEeqnVve3NPiUeQActivity"]="./../../BDD/deliveryprocesses/resources/Validar_BC3C2805_c0e462af_Activity.jpeg"
+contentPage.processPage.imageFiles["_hdKc0RFbEeqnVve3NPiUeQ,_ujWP8RFbEeqnVve3NPiUeQActivity"]="./../../BDD/deliveryprocesses/resources/Automatizar_F413E588_f8bc2032_Activity.jpeg"
+contentPage.processPage.imageFiles["_hdKc0RFbEeqnVve3NPiUeQ,_1ZqAcRFbEeqnVve3NPiUeQActivity"]="./../../BDD/deliveryprocesses/resources/Entrega_ECF401C8_f19c3c72_Activity.jpeg"
+contentPage.processPage.imageFiles["_hdKc0RFbEeqnVve3NPiUeQ,_zZaxkRFcEeqnVve3NPiUeQActivity"]="./../../BDD/deliveryprocesses/resources/Implementar_7CFE4EDF_81a68989_Activity.jpeg"
+contentPage.processPage.elementUrls["_BViHIBFdEeqnVve3NPiUeQ"] = ["Desenvolvedor","BDD/deliveryprocesses/desenvolvedor_BE5386EE.html"];
+contentPage.processPage.elementUrls["_C7tawRFdEeqnVve3NPiUeQ"] = ["Dono do produto","BDD/deliveryprocesses/dono_do_produto_24BFCA7E.html"];
+contentPage.processPage.elementUrls["_oDvIMRFbEeqnVve3NPiUeQ"] = ["Ilustrar","BDD/deliveryprocesses/Ilustrar_7B24EA6D.html"];
+contentPage.processPage.elementUrls["_RiJfsxFdEeqnVve3NPiUeQ"] = ["Engenheiro de Testes","BDD/deliveryprocesses/engenheiro_de_testes_672BD5DA.html"];
+contentPage.processPage.elementUrls["_C7tawhFdEeqnVve3NPiUeQ"] = ["Engenheiro de Testes","BDD/deliveryprocesses/engenheiro_de_testes_1358DE94.html"];
+contentPage.processPage.elementUrls["_s_f3kRFbEeqnVve3NPiUeQ"] = ["Formular","BDD/deliveryprocesses/Formular_CBB46CA0.html"];
+contentPage.processPage.elementUrls["_C7szsBFdEeqnVve3NPiUeQ"] = ["Analista de Negocios","BDD/deliveryprocesses/analista_de_negocios_35DC176C.html"];
+contentPage.processPage.elementUrls["_kKmeABFcEeqnVve3NPiUeQ"] = ["Envio do projeto","BDD/deliveryprocesses/envio_do_projeto_2B95CEFA.html"];
+contentPage.processPage.elementUrls["_zZaxkRFcEeqnVve3NPiUeQ"] = ["Implementar","BDD/deliveryprocesses/Implementar_7CFE4EDF.html"];
+contentPage.processPage.elementUrls["_kKmeARFcEeqnVve3NPiUeQ"] = ["Feedback do cliente","BDD/deliveryprocesses/feedback_do_cliente_3633AF0A.html"];
+contentPage.processPage.elementUrls["_kHYokRF1EeqnVve3NPiUeQ"] = ["Falhar no primeiro teste","BDD/deliveryprocesses/New Task Descriptor_B3803909.html"];
+contentPage.processPage.elementUrls["_C7tawBFdEeqnVve3NPiUeQ"] = ["Desenvolvedor","BDD/deliveryprocesses/desenvolvedor_1A21EA6E.html"];
+contentPage.processPage.elementUrls["_hdKc0RFbEeqnVve3NPiUeQ"] = ["Processo BDD","BDD/deliveryprocesses/Processo BDD_1733CE56.html"];
+contentPage.processPage.elementUrls["_1ZqAcRFbEeqnVve3NPiUeQ"] = ["Entrega","BDD/deliveryprocesses/Entrega_ECF401C8.html"];
+contentPage.processPage.elementUrls["_BViHIRFdEeqnVve3NPiUeQ"] = ["Engenheiro de Testes","BDD/deliveryprocesses/engenheiro_de_testes_C8F166FE.html"];
+contentPage.processPage.elementUrls["_RiJfshFdEeqnVve3NPiUeQ"] = ["Dono do produto","BDD/deliveryprocesses/dono_do_produto_5C8DF5CA.html"];
+contentPage.processPage.elementUrls["_yd4L0RFbEeqnVve3NPiUeQ"] = ["Validar","BDD/deliveryprocesses/Validar_BC3C2805.html"];
+contentPage.processPage.elementUrls["_RiJfsRFdEeqnVve3NPiUeQ"] = ["Desenvolvedor","BDD/deliveryprocesses/desenvolvedor_6DF4E1B4.html"];
+contentPage.processPage.elementUrls["_YFFoQBFdEeqnVve3NPiUeQ"] = ["Desenvolvedor","BDD/deliveryprocesses/desenvolvedor_D92B9989.html"];
+contentPage.processPage.elementUrls["_RiJfsBFdEeqnVve3NPiUeQ"] = ["Analista de Negocios","BDD/deliveryprocesses/analista_de_negocios_635701A4.html"];
+contentPage.processPage.elementUrls["_UmWQgRFdEeqnVve3NPiUeQ"] = ["Desenvolvedor","BDD/deliveryprocesses/desenvolvedor_E1512C1F.html"];
+contentPage.processPage.elementUrls["_ujWP8RFbEeqnVve3NPiUeQ"] = ["Automatizar","BDD/deliveryprocesses/Automatizar_F413E588.html"];
+contentPage.processPage.elementUrls["_i90h0BFcEeqnVve3NPiUeQ"] = ["Validacao automatizada","BDD/deliveryprocesses/validacao_automatizada_6588233B.html"];
+contentPage.processPage.elementUrls["_hAi80RFcEeqnVve3NPiUeQ"] = ["Testificar os cenarios","BDD/deliveryprocesses/testificar_os_cenarios_517E5BAB.html"];
+contentPage.processPage.elementUrls["_UmWQgBFdEeqnVve3NPiUeQ"] = ["Analista de Negocios","BDD/deliveryprocesses/analista_de_negocios_D6B34C0F.html"];
+contentPage.processPage.elementUrls["_cm4N4BFcEeqnVve3NPiUeQ"] = ["Identificar a funcionalidade do sistema","BDD/deliveryprocesses/identificar_a_funcionalidade_do_sistema_9AF89613.html"];
+contentPage.processPage.elementUrls["_eftXwBFcEeqnVve3NPiUeQ"] = ["Codificar os cenarios","BDD/deliveryprocesses/codificar_os_cenarios_B320EC31.html"];
+contentPage.processPage.elementUrls["_sr5JoBFcEeqnVve3NPiUeQ"] = ["Identificar os cenarios","BDD/deliveryprocesses/Identificar os cenarios_40E87808.html"];
+contentPage.processPage.elementUrls["_hAi80BFcEeqnVve3NPiUeQ"] = ["Testes automatizados","BDD/deliveryprocesses/testes_automatizados_46E07B9B.html"];
+contentPage.processPage.elementUrls["_kKmeAhFcEeqnVve3NPiUeQ"] = ["Implementar codigo validado","BDD/deliveryprocesses/implementar_codigo_validado_24CCC320.html"];
